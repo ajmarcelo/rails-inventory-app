85dd5699dc621e2e05875777be4fcbfc89b3a7b9
ajmarcelo <andreajeanmarcelo@gmail.com>
2025-02-20 16:47:02 +0800

    Initialization, Routes, CRUD Actions, Authentication

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,54 @@
+class ItemsController < ApplicationController
+	allow_unauthenticated_access only: %i[ index show ]
+	before_action :set_item, only: %i[ show edit update destroy ]
+
+	def index
+		@items = Item.all
+	end
+
+	# View Single Item
+	def show
+	end
+
+	# Create New Item
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		if @item.save
+			redirect_to @item
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+	
+	# Edit Item
+	def edit
+	end
+	
+	def update
+		if @item.update(item_params)
+			redirect_to @item
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
+
+	# Delete Item
+	def destroy
+		@item.destroy
+		redirect_to items_path
+	end
+
+	private
+		def set_item
+			@item = Item.find(params[:id])
+		end
+
+		# Only allow a list of trusted parameters through.
+		def item_params
+			params.expect(item: [ :name, :description, :featured_image ])
+		end
+end
