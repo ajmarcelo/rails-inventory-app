4a32adf25db2410d49ba653bb57d105976ff0864
ajmarcelo <andreajeanmarcelo@gmail.com>
2025-02-20 21:40:30 +0800

    Item and Subscribers Mail Testing

--- test/mailers/item_mailer_test.rb
+++ test/mailers/item_mailer_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 class ItemMailerTest < ActionMailer::TestCase
-  test "in_stock" do
-    mail = ItemMailer.in_stock
-    assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+    test "in_stock" do
+        mail = ItemMailer.with(item: items(:tshirt), subscriber: subscribers(:david)).in_stock
+        assert_equal "In stock", mail.subject
+        assert_equal [ "david@example.org" ], mail.to
+        assert_equal [ "from@example.com" ], mail.from
+        assert_match "Good news!", mail.body.encoded
+    end
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    include ActionMailer::TestHelper
+
+    test "sends email notifications when back in stock" do
+        item = items(:tshirt)
+
+        # Set item out of stock
+        item.update(inventory_count: 0)
+
+        assert_emails 2 do
+            item.update(inventory_count: 99)
+        end
+    end
 end
